94ef243a9b79025becd5339031aed3e2987432ab
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-08-11 13:10:57 +0300

    intial commit

---
+++ instance/config.py
@@ -0,0 +1,1 @@
+NEWS_API_KEY = ' <0a821ddbec9e472aab79c0bfd2245b41>'
---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY = '<0a821ddbec9e472aab79c0bfd2245b41>'
+export SECRET_KEY = 'BARRY'
+
+python3 run.py server
